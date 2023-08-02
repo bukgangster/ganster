67716d8b51b80127441fcdf103b1ad2edfb8a776
yangc <limyangchae@kakao.com>
2023-08-02 13:56:38 +0900

    .

---
+++ src/main/java/com/gangster/soft/GangsterApplication.java
@@ -0,0 +1,13 @@
+package com.gangster.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gangster/soft/GangsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangster.soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
